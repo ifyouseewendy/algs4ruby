33079ab5621147534eb900dd011dc634790d444b
wendi <ifyouseewendy@gmail.com>
2014-11-25 22:43:46 +0800

    Add Insertion and test

--- lib/algs4ruby.rb
+++ lib/algs4ruby.rb
@@ -1,5 +1,6 @@
 require "algs4ruby/version"
 require "algs4ruby/graph"
+require "algs4ruby/sort"
 
 module Algs4ruby
   # Your code goes here...
---
+++ lib/algs4ruby/sort/insertion.rb
@@ -0,0 +1,28 @@
+class Insertion
+  class << self
+
+    def sort(array)
+      arr = array.clone
+      n = arr.length
+
+      0.upto(n-2) do |i|
+        (i+1).downto(1) do |j|
+          exch(arr, j, j-1) if less(arr[j], arr[j-1])
+        end
+      end
+
+      arr
+    end
+
+    def less(a, b)
+      a < b
+    end
+
+    def exch(arr, i, j)
+      t = arr[i]
+      arr[i] = arr[j]
+      arr[j] = t
+    end
+
+  end
+end
