277978441e4504a10ac8d224268ec9808fc40838
wendi <ifyouseewendy@gmail.com>
2014-11-25 22:43:46 +0800

    Add Insertion and test

--- lib/algs4ruby.rb
+++ lib/algs4ruby.rb
@@ -1,4 +1,6 @@
 require "algs4ruby/version"
+require "algs4ruby/graph"
+require "algs4ruby/sort"
 
 module Algs4ruby
   # Your code goes here...
--- test/sort/insertion_test.rb
+++ test/sort/insertion_test.rb
@@ -1,10 +1,14 @@
 require 'test_helper'
+require 'algs4ruby/sort/insertion'
 
-class TestDemo < MiniTest::Unit::TestCase
+class InsertionTest < MiniTest::Unit::TestCase
   def setup
-    @msg = 'hello world'
+    # 10.times.reduce([]){|ar, i| ar << rand(10) }.join(', ')
+    @array = [9, 3, 5, 2, 0, 6, 4, 5, 3, 5]
   end
-  def test_hello_world
-    assert_equal 'hello world', @msg
+
+  def test_sort
+    assert_equal @array.sort, Insertion.sort(@array)
+    assert_equal [9, 3, 5, 2, 0, 6, 4, 5, 3, 5], @array
   end
 end
