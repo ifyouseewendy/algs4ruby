305ee6ae7b2cc5a102d095770e54e93060c10d2b
wendi <ifyouseewendy@gmail.com>
2014-12-02 11:46:58 +0800

    Improve Merge sort
    1. add cutoff, use Insertion for small arrays
    2. no merge if already sorted

--- lib/algs4ruby/sort/merge.rb
+++ lib/algs4ruby/sort/merge.rb
@@ -1,14 +1,28 @@
+require_relative 'insertion'
+
 class Merge
   class << self
 
+    ##
+    # OPTI
+    #
+    # 1. Add cutoff, use Insertion for small arrays
+    # 2. No merge if already sorted
+    #
+
+    CUTOFF = 7
+
     def sort(array)
-      return array if array.count == 1
+      # by Default, return array if array.count == 1
+      return Insertion.sort(array) if array.count <= CUTOFF
 
       low, high = 0, array.length-1
       mid = (low+high)/2
 
       first   = sort(array[low..mid])
       second  = sort(array[mid+1..high])
+
+      return array if less(first[-1], second[0])
       merge(first, second)
     end
 
