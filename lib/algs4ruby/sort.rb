0fc12fa4e3d50b3e42d570b53994d36bd0e97fa8
wendi <ifyouseewendy@gmail.com>
2014-12-03 23:58:28 +0800

    Extract Sorting::Base

--- lib/algs4ruby/sort.rb
+++ lib/algs4ruby/sort.rb
@@ -1,3 +1,26 @@
+module Sorting
+  class Base
+    class << self
+      def sort(array, strategy = nil, &block)
+        raise 'Not Implemented'
+      end
+
+      private
+
+        def less(a, b, &block)
+          block_given? ? yield(a,b) : a < b
+        end
+
+        def exch(arr, i, j)
+          t       = arr[i]
+          arr[i]  = arr[j]
+          arr[j]  = t
+        end
+    end
+
+  end
+end
+
 require_relative 'sort/selection'
 require_relative 'sort/insertion'
 require_relative 'sort/shell'
