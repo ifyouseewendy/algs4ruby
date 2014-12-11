e57e25f04229624b3e2a079f14f40a03de2c591f
wendi <ifyouseewendy@gmail.com>
2014-12-11 15:40:06 +0800

    Add in-place vs. out-of-place comment

--- lib/algs4ruby/sorting.rb
+++ lib/algs4ruby/sorting.rb
@@ -14,6 +14,39 @@ module Algs4ruby
 
     class Base
       class << self
+        # = In-place vs. Out-of-place
+        #
+        #   I was coding all kinds of sorting methods in an out-of-place way,
+        #   which means no care about the auxiliary space or relative operations.
+        #   To be specific:
+        #
+        #     1. Manipulate and return a cloned array, keeping the array passed in immutable.
+        #     2. Writing more natural code by leveraging Ruby, without efficiency concern.
+        #        For instance in Merge Sort:
+        #
+        #         def recursive_sort(array
+        #           ...
+        #
+        #           first   = recursive_sort(array[low..mid])
+        #           second  = recursive_sort(array[mid+1..high])
+        #           merge(first, second)
+        #         end
+        #
+        #         def merge(first, second)
+        #           return first + second if ...
+        #           ...
+        #         end
+        #
+        #   Although Merge Sort does need an auxiliary linear space, but maybe I get a little
+        #   overhead. Especially when I learned *in-place* (uses only a small auxiliary stack)
+        #   is mainly the advantage over Merge Sort, I knew I need a refactor work. Here is my
+        #   final thought:
+        #
+        #     1. Preserve the clone process, no modifying the argument array.
+        #     2. Do all in-place change on cloned array.
+        #
+        #   status: TODO @2014-12-11
+
         def sort(array, strategy = nil, &block)
           raise 'Not Implemented'
         end
