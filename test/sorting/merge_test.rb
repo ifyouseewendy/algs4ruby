b2d018688a05e74b51a43cb7fd996e3b7d012609
wendi <ifyouseewendy@gmail.com>
2016-03-16 10:44:30 +0800

    sorting: Add merge test for bottom_up strategy

--- test/sorting/merge_test.rb
+++ test/sorting/merge_test.rb
@@ -5,9 +5,41 @@ module Algs4ruby
     class MergeTest < MiniTest::Unit::TestCase
       include SortingTestHelper
 
+      attr_reader :sorting, :array, :origin_array
+
+      def setup
+        @sorting      = Sorting::Merge.new
+
+        @array        = [9, 3, 5, 2, 0, 6, 4, 5, 3, 5]
+        @origin_array = @array
+        super
+      end
+
+      def test_sort
+        expected = array.sort
+        assert_equal expected, sorting.sort(array)
+        assert_equal origin_array, array
+      end
+
+      def test_sort!
+        expected = array.sort
+        assert_nil sorting.sort!(array)
+        assert_equal expected, array
+      end
+
+      def test_sort_by_specific_order
+        assert_equal array.sort.reverse, sorting.sort(array){|a,b| a > b}
+      end
+
+      def test_another_strategy
+        expected = array.sort
+        assert_equal expected, sorting.sort(array, :bottom_up)
+      end
+
       def test_sort_stability
         assert_stable(:merge)
       end
+
     end
   end
 end
